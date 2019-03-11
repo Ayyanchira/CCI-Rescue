04097f62a7ab47dab5b24791e3df721977b42f15
Rinita Nair <rnair3@uncc.edu>
2019-03-11 00:46:13 -0400

    Enemy movement and basic navmesh

---
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform destination;
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if(navMeshAgent == null)
+        {
+            Debug.Log("error");
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 target = destination.transform.position;
+            navMeshAgent.SetDestination(target);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
