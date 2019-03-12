811880624b4c8f101ffbfcf10a09f365f9622a0b
Rinita Nair <rnair3@uncc.edu>
2019-03-12 00:15:04 -0400

    Enemy Patrolling

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -6,8 +6,21 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField] Transform destination;
+    //[SerializeField] Transform destination;
     NavMeshAgent navMeshAgent;
+
+
+    [SerializeField] List<Waypoints> patrolPoints;
+    [SerializeField] bool patrolWaiting;
+    [SerializeField] float totalWaitTime = 3f;
+    [SerializeField] float switchProbability = .2f;
+
+    int currentPatrolIndex;
+    bool travel = true;
+    bool wait;
+    bool patrolForward;
+    float waitTimer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,22 +32,77 @@ public class EnemyMovement : MonoBehaviour
         }
         else
         {
-            SetDestination();
+            if (patrolPoints != null && patrolPoints.Count >= 2)
+            {
+                currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Insufficient patrol points for basic patrolling behaviour.");
+            }
+
         }
     }
 
     private void SetDestination()
     {
-        if(destination != null)
+        if(patrolPoints != null)
         {
-            Vector3 target = destination.transform.position;
+            Vector3 target = patrolPoints[currentPatrolIndex].transform.position;
             navMeshAgent.SetDestination(target);
+            travel = true;
         }
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        Debug.Log(currentPatrolIndex);
+        if(travel && navMeshAgent.remainingDistance <= 1.0f)
+        {
+            travel = false;
+            if (patrolWaiting)
+            {
+                wait = true;
+                waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+        if (wait)
+        {
+            waitTimer += Time.deltaTime;
+            if(waitTimer >= totalWaitTime)
+            {
+                wait = false;
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if(UnityEngine.Random.Range(0f, 1f) <= switchProbability)
+        {
+            patrolForward = !patrolForward;
+
+        }
+        if (patrolForward)
+        {
+            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Count;
+
+        }
+        else
+        {
+            if(--currentPatrolIndex < 0)
+            {
+                currentPatrolIndex = patrolPoints.Count - 1;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/EnemyScripts/Waypoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private float radius = 1.0f;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
